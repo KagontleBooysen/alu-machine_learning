ae62d46db71410ab73e73b3bd5fec3e6c5ff75f7
KagontleBooysen <k.booysen@alustudent.com>
2024-05-17 16:23:20 -0700

    6-neural_style.py

--- supervised_learning/neural_style_transfer/6-neural_style.py
+++ supervised_learning/neural_style_transfer/6-neural_style.py
@@ -16,10 +16,10 @@ class NST:
         tf.enable_eager_execution()
         if (type(style_image) is not np.ndarray or style_image.ndim != 3 or
                 style_image.shape[2] != 3):
-            raise TypeError('style_image must be a numpy.ndarray with shape (h, w, 3)')
+            raise TypeError('style_image numpy.ndarray  shape (h, w, 3)')
         if (type(content_image) is not np.ndarray or content_image.ndim != 3 or
                 content_image.shape[2] != 3):
-            raise TypeError('content_image must be a numpy.ndarray with shape (h, w, 3)')
+            raise TypeError('content_image mus numpy.ndarray  shape (h, w, 3)')
         if (type(alpha) is not int and type(alpha) is not float) or alpha < 0:
             raise TypeError('alpha must be a non-negative number')
         if (type(beta) is not int and type(beta) is not float) or beta < 0:
@@ -35,7 +35,7 @@ class NST:
     def scale_image(image):
         if (type(image) is not np.ndarray or image.ndim != 3 or
                 image.shape[2] != 3):
-            raise TypeError('image must be a numpy.ndarray with shape (h, w, 3)')
+            raise TypeError('image must be a numpy.ndarray shape (h, w, 3)')
         max_dims = 512
         shape = image.shape[:2]
         scale = max_dims / max(shape[0], shape[1])
@@ -46,7 +46,8 @@ class NST:
         return image
 
     def load_model(self):
-        vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
+        vgg = tf.keras.applications.vgg19.VGG19(
+                include_top=False, weights='imagenet')
         x = vgg.input
         model_outputs = []
         content_output = None
@@ -69,7 +70,7 @@ class NST:
     @staticmethod
     def gram_matrix(input_layer):
         if not (isinstance(input_layer, tf.Tensor) or
-                isinstance(input_layer, tf.Variable)) or input_layer.shape.ndims != 4:
+                isinstance(input_la, tf.Variabl)) or input_la.shape.ndims != 4:
             raise TypeError('input_layer must be a tensor of rank 4')
         _, nh, nw, _ = input_layer.shape.dims
         G = tf.linalg.einsum('bijc,bijd->bcd', input_layer, input_layer)
@@ -83,23 +84,25 @@ class NST:
         style_features = self.model(preprocessed_s)[:-1]
         self.content_feature = self.model(preprocessed_c)[-1]
         self.gram_style_features = [
-            self.gram_matrix(style_feature) for style_feature in style_features]
+            self.gram_matrix(style_feature) for style_feature in style_feature]
 
     def layer_style_cost(self, style_output, gram_target):
         if not (isinstance(style_output, tf.Tensor) or
-                isinstance(style_output, tf.Variable)) or style_output.shape.ndims != 4:
+                isinstance(sty_output, tf.Var)) or sty_output.shape.ndims != 4:
             raise TypeError('style_output must be a tensor of rank 4')
         m, _, _, nc = style_output.shape.dims
         if not (isinstance(gram_target, tf.Tensor) or
-                isinstance(gram_target, tf.Variable)) or gram_target.shape.dims != [m, nc, nc]:
-            raise TypeError('gram_target must be a tensor of shape [{}, {}, {}]'.format(
+                isinstance(gram_target, tf.Variable)) or 
+        gram_target.shape.dims != [m, nc, nc]:
+            raise TypeError('gram_target  tensor of shape [{}, {}, {}]'.format(
                 m, nc, nc))
         gram_style = self.gram_matrix(style_output)
-        return tf.reduce_sum(tf.square(gram_style - gram_target)) / tf.square(tf.cast(nc, tf.float32))
+        return tf.reduce_sum(tf.square(gram_style - gram_target)) / tf.square(
+                tf.cast(nc, tf.float32))
 
     def style_cost(self, style_outputs):
-        if type(style_outputs) is not list or len(style_outputs) != len(self.style_layers):
-            raise TypeError('style_outputs must be a list with a length of {}'.format(
+        if type(sty_puts) is not list or len(sty_puts) != len(self.sty_layer):
+            raise TypeError('style_outputs must be a list length {}'.format(
                 len(self.style_layers)))
         J_style = tf.add_n([self.layer_style_cost(style_outputs[i],
                                                   self.gram_style_features[i])
@@ -109,10 +112,10 @@ class NST:
 
     def content_cost(self, content_output):
         if not (isinstance(content_output, tf.Tensor) or
-                isinstance(content_output, tf.Variable)) or content_output.shape.dims != self.content_feature.shape.dims:
-            raise TypeError('content_output must be a tensor of shape {}'.format(
+                isinstance(content_output, tf.Variable)) or 
+        content_output.shape.dims != self.content_feature.shape.dims:
+            raise TypeError('content_output must tensor of shape {}'.format(
                 self.content_feature.shape))
         _, nh, nw, nc = content_output.shape.dims
-        return tf.reduce_sum(tf.square(content_output - self.content_feature)) / tf.cast(
+        return tf.reduce_sum(tf.square(cont_output - self.cont_feature)) / tf.cast(
             nh * nw * nc, tf.float32)
-
