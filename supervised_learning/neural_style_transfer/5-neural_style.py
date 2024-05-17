53507d5ce77b0a4214268884f9ff7a13d20c8e0d
KagontleBooysen <k.booysen@alustudent.com>
2024-05-17 14:15:24 -0700

    5-neural_style.py

--- supervised_learning/neural_style_transfer/5-neural_style.py
+++ supervised_learning/neural_style_transfer/5-neural_style.py
@@ -3,7 +3,6 @@ import numpy as np
 import tensorflow as tf
 
 class NST:
-    
     style_layers = ['block1_conv1',
                     'block2_conv1',
                     'block3_conv1', 
@@ -12,14 +11,14 @@ class NST:
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        tf.enable_eager_execution()
-        if type(style_image) is not np.ndarray or style_image.ndim != 3 or style_image.shape[2] != 3:
+        """Initialize NST class with style and content images, alpha and beta."""
+        if not self._is_valid_image(style_image):
             raise TypeError('style_image must be a numpy.ndarray with shape (h, w, 3)')
-        if type(content_image) is not np.ndarray or content_image.ndim != 3 or content_image.shape[2] != 3:
+        if not self._is_valid_image(content_image):
             raise TypeError('content_image must be a numpy.ndarray with shape (h, w, 3)')
-        if (type(alpha) is not int and type(alpha) is not float) or alpha < 0:
+        if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError('alpha must be a non-negative number')
-        if (type(beta) is not int and type(beta) is not float) or beta < 0:
+        if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError('beta must be a non-negative number')
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
@@ -29,19 +28,26 @@ class NST:
         self.generate_features()
 
     @staticmethod
+    def _is_valid_image(image):
+        """Check if the image is a valid numpy array with shape (h, w, 3)."""
+        return isinstance(image, np.ndarray) and image.ndim == 3 and image.shape[2] == 3
+
+    @staticmethod
     def scale_image(image):
-        if type(image) is not np.ndarray or image.ndim != 3 or image.shape[2] != 3:
+        """Scale and preprocess the image."""
+        if not NST._is_valid_image(image):
             raise TypeError('image must be a numpy.ndarray with shape (h, w, 3)')
         max_dims = 512
         shape = image.shape[:2]
         scale = max_dims / max(shape[0], shape[1])
         new_shape = (int(scale * shape[0]), int(scale * shape[1]))
         image = np.expand_dims(image, axis=0)
-        image = tf.clip_by_value(tf.image.resize_bicubic(image, new_shape) / 255.0, 0.0, 1.0)
+        image = tf.clip_by_value(tf.image.resize(image, new_shape, method='bicubic') / 255.0, 0.0, 1.0)
         return image
 
     def load_model(self):
-        vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
+        """Load the VGG19 model and modify it for NST."""
+        vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
         x = vgg.input
         model_outputs = []
         content_output = None
@@ -56,11 +62,11 @@ class NST:
                     content_output = x
                 layer.trainable = False
         model_outputs.append(content_output)
-        model = tf.keras.models.Model(vgg.input, model_outputs)
-        self.model = model
+        self.model = tf.keras.models.Model(vgg.input, model_outputs)
 
     @staticmethod
     def gram_matrix(input_layer):
+        """Compute the Gram matrix for a given input layer."""
         if not (isinstance(input_layer, tf.Tensor) or isinstance(input_layer, tf.Variable)) or input_layer.shape.ndims != 4:
             raise TypeError('input_layer must be a tensor of rank 4')
         _, nh, nw, _ = input_layer.shape.dims
@@ -68,6 +74,7 @@ class NST:
         return G / tf.cast(nh * nw, tf.float32)
 
     def generate_features(self):
+        """Generate the style and content features."""
         preprocessed_s = tf.keras.applications.vgg19.preprocess_input(self.style_image * 255)
         preprocessed_c = tf.keras.applications.vgg19.preprocess_input(self.content_image * 255)
         style_features = self.model(preprocessed_s)[:-1]
@@ -75,17 +82,16 @@ class NST:
         self.gram_style_features = [self.gram_matrix(style_feature) for style_feature in style_features]
 
     def layer_style_cost(self, style_output, gram_target):
+        """Compute the style cost for a single layer."""
         if not (isinstance(style_output, tf.Tensor) or isinstance(style_output, tf.Variable)) or style_output.shape.ndims != 4:
             raise TypeError('style_output must be a tensor of rank 4')
-        m, _, _, nc = style_output.shape.dims
-        if not (isinstance(gram_target, tf.Tensor) or isinstance(gram_target, tf.Variable)) or gram_target.shape.dims != [m, nc, nc]:
-            raise TypeError('gram_target must be a tensor of shape [{}, {}, {}]'.format(m, nc, nc))
         gram_style = self.gram_matrix(style_output)
-        return tf.reduce_sum(tf.square(gram_style - gram_target)) / tf.square(tf.cast(nc, tf.float32))
+        return tf.reduce_sum(tf.square(gram_style - gram_target)) / tf.square(tf.cast(gram_style.shape[-1], tf.float32))
 
     def style_cost(self, style_outputs):
-        if type(style_outputs) is not list or len(style_outputs) != len(self.style_layers):
-            raise TypeError('style_outputs must be a list with a length of {}'.format(len(self.style_layers)))
+        """Compute the overall style cost."""
+        if not isinstance(style_outputs, list) or len(style_outputs) != len(self.style_layers):
+            raise TypeError(f'style_outputs must be a list with a length of {len(self.style_layers)}')
         J_style = tf.add_n([self.layer_style_cost(style_outputs[i], self.gram_style_features[i]) for i in range(len(style_outputs))])
         J_style /= tf.cast(len(style_outputs), tf.float32)
         return J_style
