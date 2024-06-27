f2675a17787beed4a9ad1a77a68efae8d8c00bab
KagontleBooysen <k.booysen@alustudent.com>
2024-06-26 19:51:49 -0700

    4-P_affinities.py

--- unsupervised_learning/dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -1,47 +1,52 @@
 #!/usr/bin/env python3
-"""Function that calculates the Shannon entropy
-and P affinities relative to a data point"""
+"""Shannon entropy and P affinities"""
 
 import numpy as np
-
-
 P_init = __import__('2-P_init').P_init
 HP = __import__('3-entropy').HP
 
 
 def P_affinities(X, tol=1e-5, perplexity=30.0):
-    """X is a numpy.ndarray of shape (n, d)
-    containing the dataset to be transformed by t-SNE
-        n is the number of data points
-        d is the number of dimensions in each point
-    perplexity is the perplexity that all Gaussian distributions should have
-    tol is the maximum tolerance allowed (inclusive) for the difference
-    in Shannon entropy from perplexity for all Gaussian distributions
-    You should use P_init = __import__('2-P_init').P_init
-    and HP = __import__('3-entropy').HP
-    Returns: P, a numpy.ndarray of shape (n, n)
-    containing the symmetric P affinities"""
+    """
+    Binary search to get P affinities so each gaussian
+    has the same perplexity
+    Args:
+        X:
+        tol:
+        perplexity:
+    Returns:
+    """
+    (n, d) = X.shape
     D, P, betas, H = P_init(X, perplexity)
-    for i in range(X.shape[0]):
-        Di = np.append(D[i, :i], D[i, i+1:])
-        Hi, Pi = HP(Di, betas[i])
-        bmin = None
-        bmax = None
+
+    if n == 0:
+        return P
+
+    for i in range(n):
+        row = D[i].copy()
+        row = np.delete(row, i, axis=0)
+        Hi, Pi = HP(row, betas[i])
         Hdiff = Hi - H
+        b_max = None
+        b_min = None
         while np.abs(Hdiff) > tol:
             if Hdiff > 0:
-                bmin = betas[i].copy()
-                if bmax is None:
-                    betas[i] = betas[i] * 2
+                b_min = betas[i, 0]
+                if b_max is None:
+                    betas[i, 0] = betas[i, 0] * 2.
                 else:
-                    betas[i] = (betas[i] + bmax) / 2
+                    betas[i, 0] = (betas[i, 0] + b_max) / 2.
             else:
-                bmax = betas[i].copy()
-                if bmin is None:
-                    betas[i] = betas[i] / 2
+                b_max = betas[i, 0]
+                if b_min is None:
+                    betas[i, 0] = betas[i, 0] / 2.
                 else:
-                    betas[i] = (betas[i] + bmin) / 2
-            Hi, Pi = HP(Di, betas[i])
+                    betas[i, 0] = (betas[i, 0] + b_min) / 2.
+
+            Hi, Pi = HP(row, betas[i])
             Hdiff = Hi - H
-        P[i, np.concatenate((np.r_[0:i], np.r_[i+1:X.shape[0]]))] = Pi
-    return (P.T + P) / (2 * X.shape[0])
+        Pi = np.insert(Pi, i, 0)
+        P[i] = Pi
+
+    P = (P.T + P) / (2 * n)
+    return P
