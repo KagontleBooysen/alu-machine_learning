f4c038b4ec4518ac9d9b225723c238045d420e75
KagontleBooysen <k.booysen@alustudent.com>
2024-06-26 19:54:22 -0700

    4-P_affinities.py

--- unsupervised_learning/dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -1,52 +1,39 @@
 #!/usr/bin/env python3
-"""Shannon entropy and P affinities"""
-
+""" dimensionality reduction"""
 import numpy as np
 P_init = __import__('2-P_init').P_init
 HP = __import__('3-entropy').HP
 
 
 def P_affinities(X, tol=1e-5, perplexity=30.0):
-    """
-    Binary search to get P affinities so each gaussian
-    has the same perplexity
-    Args:
-        X:
-        tol:
-        perplexity:
-    Returns:
-    """
-    (n, d) = X.shape
-    D, P, betas, H = P_init(X, perplexity)
-
-    if n == 0:
-        return P
-
+    """calculates the symmetric P affinities of a data set"""
+    # (2500, 2500), (2500, 2500), (2500, 1), 4.906890595608519
+    Dnorm, P, betas, H = P_init(X, perplexity)
+    n, d = X.shape
     for i in range(n):
-        row = D[i].copy()
-        row = np.delete(row, i, axis=0)
-        Hi, Pi = HP(row, betas[i])
-        Hdiff = Hi - H
-        b_max = None
-        b_min = None
+        copy = Dnorm[i]
+        copy = np.delete(copy, i, axis=0)
+        hi, pi = HP(copy, betas[i])
+        betamin = None
+        betamax = None
+        Hdiff = hi - H
+
         while np.abs(Hdiff) > tol:
             if Hdiff > 0:
-                b_min = betas[i, 0]
-                if b_max is None:
-                    betas[i, 0] = betas[i, 0] * 2.
+                betamin = betas[i, 0]
+                if betamax is None:
+                    betas[i] = betas[i] * 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + b_max) / 2.
+                    betas[i] = (betas[i] + betamax) / 2
             else:
-                b_max = betas[i, 0]
-                if b_min is None:
-                    betas[i, 0] = betas[i, 0] / 2.
+                betamax = betas[i, 0]
+                if betamin is None:
+                    betas[i] = betas[i] / 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + b_min) / 2.
-
-            Hi, Pi = HP(row, betas[i])
-            Hdiff = Hi - H
-        Pi = np.insert(Pi, i, 0)
-        P[i] = Pi
-
-    P = (P.T + P) / (2 * n)
+                    betas[i] = (betas[i] + betamin) / 2
+            hi, pi = HP(copy, betas[i])
+            Hdiff = hi - H
+        aux = np.insert(pi, i, 0)
+        P[i] = aux
+    P = (P.T + P) / (2*n)
     return P
