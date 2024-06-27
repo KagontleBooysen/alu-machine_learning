c3567f6d306130d6590b41a889f2bcc4f9ba7f48
KagontleBooysen <k.booysen@alustudent.com>
2024-06-26 19:56:08 -0700

    6-grads.py

--- unsupervised_learning/dimensionality_reduction/6-grads.py
+++ unsupervised_learning/dimensionality_reduction/6-grads.py
@@ -1,29 +1,16 @@
 #!/usr/bin/env python3
-"""
-Defines function that calculates the gradients of Y
-"""
-
-
+""" dimensionality reduction"""
 import numpy as np
 Q_affinities = __import__('5-Q_affinities').Q_affinities
 
 
 def grads(Y, P):
-    """
-    Calculates the gradients of Y
-
-    parameters:
-        Y [numpy.ndarray of shape (n, ndim)]:
-            containing the low dimensional transformation of X (dataset)
-            n: the number of data points
-            ndim: the new dimensional representation of X
-        P [numpy.ndarray of shape (n, n)]:
-            containing the P affinities of X
-
-    returns:
-        dy [numpy.ndarray of shape (n, ndim)]:
-            contatining the gradients of Y
-        Q [numpy.ndarray of shape (n, n)]:
-            containing the Q affinities of Y
-    """
-    return None
+    """calculates the gradients of Y"""
+    n, d = Y.shape
+    Q, num = Q_affinities(Y)
+    z = np.zeros((n, d))
+    PQ = P - Q
+    for i in range(n):
+        z[i, :] = np.sum(
+            np.tile(PQ[:, i] * num[:, i], (d, 1)).T * (Y[i, :] - Y), 0)
+    return z, Q
