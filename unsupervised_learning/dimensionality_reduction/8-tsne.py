b6e0b66db518479e38c0e7f522f4447616d04944
KagontleBooysen <k.booysen@alustudent.com>
2024-06-29 16:47:43 -0700

    8-tsne.py

--- unsupervised_learning/dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/dimensionality_reduction/8-tsne.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python3
-""" This module has the method Q_affinities(Y)"""
+
+
+"""useless comment"""
+
 
 import numpy as np
 
+pca = __import__('1-pca').pca
+P_affinities = __import__('4-P_affinities').P_affinities
+grads = __import__('6-grads').grads
+cost = __import__('7-cost').cost
 
-def Q_affinities(Y):
+
+def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
     """
-    Y is a numpy.ndarray of shape (n, ndim) containing the low
-    dimensional transformation of X n is the number of points
-    ndim is the new dimensional representation of X
-    Returns: Q, num Q is a numpy.ndarray of shape (n, n)
-    containing the Q affinities num is a numpy.ndarray of
-    shape (n, n) containing the numerator of the Q affinities
+    Compute the t-sne dimension reduction
+    :param X: Containing the dataset to be transformed by t-SNE
+    :param ndims: New dimensional representation of X
+    :param idims: Intermediate dimensional representation of X after PCA
+    :param perplexity: Is the perplexity
+    :param iterations: Is the number of iterations
+    :param lr: Is the learning rate
+    :return: Y containing the optimized low dimensional transformation of X
     """
-    sum_Y = np.sum(np.square(Y), 1)
-    Di = np.add(np.add(-2 * np.dot(Y, Y.T), sum_Y).T, sum_Y)
-    np.fill_diagonal(Di, 0)
-    num = 1 / (1 + Di)
-    np.fill_diagonal(num, 0)
-    Qi = num / np.sum(num)
-    return Qi, num
+    momentum_coeff = 0.8
+    n, d = X.shape
+
+    pca_res = pca(X, idims)
+    P = P_affinities(pca_res, perplexity=perplexity)
+    P *= 4
+
+    Y = []
+    y = np.random.randn(n, ndims)
+    Y.append(y)
+    Y.append(y)
+
+    for i in range(iterations):
+        dY, Q = grads(Y[-1], P)
+        y = Y[-1] - lr * dY + momentum_coeff * (Y[-1] - Y[-2])
+        y = y - np.tile(np.mean(y, 0), (n, 1))
+        Y.append(y)
+
+        if (i + 1) % 100 == 0:
+            current_cost = cost(P, Q)
+            print("Cost at iteration {}: {}".format(i + 1, current_cost))
+
+        if i == 20:
+            momentum_coeff = 0.5
+
+        if (i + 1) == 100:
+            P /= 4
+
+    return Y[-1]
