9e56b487e47e6920b2d27b852cbfe04dfdca64e7
KagontleBooysen <k.booysen@alustudent.com>
2024-06-29 15:51:47 -0700

    5-Q_affinities.py

--- unsupervised_learning/dimensionality_reduction/5-Q_affinities.py
+++ unsupervised_learning/dimensionality_reduction/5-Q_affinities.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 """
-Defines function that calculates the Q affinities
+Calculates the symmetric Q affinities
 """
-
-
 import numpy as np
 
 
 def Q_affinities(Y):
     """
-    Calculates the Q affinitiest
-
-    parameters:
-        Y [numpy.ndarray of shape (n, ndim)]:
-            containing the low dimensional transformation of X (dataset)
-            n: the number of data points
-            ndim: the new dimensional representation of X
-
-    returns:
-        Q [numpy.ndarray of shape (n, n)]:
-            contatining the Q affinities
-        num [numpy.ndarray of shape (n, n)]:
-            containing the numerator of the Q affinities
+    Calculates the Q affinities
+    :param Y: Y is a numpy.ndarray of shape (n, ndim) containing the low
+    dimensional transformation of X
+        n is the number of points
+        ndim is the new dimensional representation of X
+    :return: Q, num:
+        Q is a numpy.ndarray of shape (n, n) containing the Q affinities
+        num is a numpy.ndarray of shape (n, n) containing the numerator of the
+        Q affinities
     """
-    return None
+    sum_Y = np.sum(np.square(Y), 1)
+    num = -2 * np.dot(Y, Y.T)
+    num = 1 / (1. + np.add(np.add(num, sum_Y).T, sum_Y))
+    np.fill_diagonal(num, 0)
+    Q = num / np.sum(num)
+
+    return Q, num
