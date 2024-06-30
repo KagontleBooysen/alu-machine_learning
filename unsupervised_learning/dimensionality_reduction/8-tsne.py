ef917f41c3dc401eabe627eddcb2e8260e2c72ba
KagontleBooysen <k.booysen@alustudent.com>
2024-06-30 10:52:48 -0700

    8-tsne.py

--- unsupervised_learning/dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/dimensionality_reduction/8-tsne.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""t-SNE method"""
+"""t-sne"""
 
 import numpy as np
 pca = __import__('1-pca').pca
@@ -9,53 +9,56 @@ cost = __import__('7-cost').cost
 
 
 def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
-    """calculates t-SNE transformation:
-    Arg:
-       - X: np.ndarray shape (n, d) containing the dataset to be transform:
-             - n is the number of data points
-             - d is the number of dimensions in each point
-       - ndims is the new dimensional representation of X
-       - idims is the intermediate dimensional representation of X after PCA
-       - perplexity is the perplexity
-       - iterations is the number of iterations
-       - lr is the learning rate
-
-    Returns:
-       - Y: np.ndarray of shape (n, ndim) containing the optimized low
-            dimensional transformation of X
+    """
+    T-sne function
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset to be
+           transformed by t-SNE
+        ndims: the new dimensional representation of X
+        idims: the intermediate dimensional representation of X after
+                PCA
+        perplexity:  perplexity
+        iterations: number of iterations
+        lr: learning rate
+    Returns: Y, a numpy.ndarray of shape (n, ndim) containing the optimized
+             low dimensional
     """
     n, d = X.shape
     initial_momentum = 0.5
     final_momentum = 0.8
 
+    # min_gain = 0.01
+    # gains = np.ones((n, ndims))
+
     X = pca(X, idims)
     P = P_affinities(X, perplexity=perplexity)
-    # early exageration
-    P = P * 4
-
     Y = np.random.randn(n, ndims)
-    iY = np.zeros((n, ndims))
+    iY = Y
+    # early exaggeration
+    P = P * 4.
 
     for i in range(iterations):
 
         dY, Q = grads(Y, P)
-        if i < (20):
+        if i <= 20:
             momentum = initial_momentum
         else:
             momentum = final_momentum
 
-        # perform the update
+        # delta-bar-delta algorithm for SDG optional
+        # gains = (gains + 0.2) * ((dY > 0.) != (iY > 0.)) + \
+        #        (gains * 0.8) * ((dY > 0.) == (iY > 0.))
+        # gains[gains < min_gain] = min_gain"""
+
         iY = momentum * iY - lr * dY
         Y = Y + iY
         Y = Y - np.tile(np.mean(Y, 0), (n, 1))
 
-        # print cost of the T SEN model
         if (i + 1) % 100 == 0:
             C = cost(P, Q)
-            print("Cost at iteration {}: {}".format((i+1), C))
+            print('Cost at iteration {}: {}'.format((i+1), C))
 
-        # perform the aerly exagerations for first 100 iterations
-        if (i + 1) == 100:
-            P = P / 4
+        if i == 100:
+            P = P / 4.
 
-    return (Y)
+    return Y
