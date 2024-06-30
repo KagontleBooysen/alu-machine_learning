2f2ff32a1004f4389c5691d7466ee604d26a3d34
KagontleBooysen <k.booysen@alustudent.com>
2024-06-30 02:03:33 -0700

    9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+
 """useless comment"""
+
 
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
+
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """calculates BIC over various """
@@ -24,21 +27,15 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
     if type(verbose) is not bool:
         return None, None, None, None
-    
     b = np.zeros(kmax + 1 - kmin)
     log_likelihood = np.zeros(kmax + 1 - kmin)
     results = []
-    
     for k in range(kmin, kmax + 1):
-        pi, m, S, _, log_likelihood[k - kmin] = expectation_maximization(
-            X, k, iterations=iterations, tol=tol, verbose=verbose
-        )
+        pi, m, S, _, log_likelihood[k - kmin] = expectation_maximization(X, k, iterations=iterations, tol=tol, verbose=verbose)
         results.append((pi, m, S))
         p = k * (d + 2) * (d + 1) / 2 - 1
         b[k - kmin] = p * np.log(n) - 2 * log_likelihood[k - kmin]
-    
     amin = np.argmin(b)
     best_k = amin + kmin
     best_result = results[amin]
-    
     return best_k, best_result, log_likelihood, b
