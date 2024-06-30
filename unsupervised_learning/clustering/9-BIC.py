d25974dd88e9335a8ca184f8e26811818629b695
KagontleBooysen <k.booysen@alustudent.com>
2024-06-30 03:01:18 -0700

    9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-
 """useless comment"""
-
 
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
-
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """calculates BIC over various """
@@ -21,22 +18,29 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
     if kmax <= kmin:
         return None, None, None, None
-    if (type(iterations) is not int or iterations != int(iterations) or iterations < 1):
+    if (type(iterations) is not int or
+        iterations != int(iterations) or
+        iterations < 1):
         return None, None, None, None
     if type(tol) is not float or tol < 0:
         return None, None, None, None
     if type(verbose) is not bool:
         return None, None, None, None
+    
     b = np.zeros(kmax + 1 - kmin)
     log_likelihood = np.zeros(kmax + 1 - kmin)
     results = []
+    
     for k in range(kmin, kmax + 1):
         pi, m, S, _, log_likelihood[k - kmin] = expectation_maximization(
-                X, k, iterations=iterations, tol=tol, verbose=verbose)
+            X, k, iterations=iterations, tol=tol, verbose=verbose
+        )
         results.append((pi, m, S))
         p = k * (d + 2) * (d + 1) / 2 - 1
         b[k - kmin] = p * np.log(n) - 2 * log_likelihood[k - kmin]
+    
     amin = np.argmin(b)
     best_k = amin + kmin
     best_result = results[amin]
+    
     return best_k, best_result, log_likelihood, b
