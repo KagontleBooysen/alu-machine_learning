54dd09e84ab48bcb541dd3ddd4e7ac5f4508a115
KagontleBooysen <k.booysen@alustudent.com>
2024-06-30 03:37:23 -0700

    9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-"""Calculates BIC over various cluster counts."""
+"""Calculates BIC."""
 
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
+
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """Calculates BIC for various cluster counts."""
