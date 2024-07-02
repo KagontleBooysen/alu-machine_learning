0723f204af199ce1e2724b4fbf73e9a8b1547d8c
KagontleBooysen <k.booysen@alustudent.com>
2024-07-02 09:16:50 -0700

    2-absorbing.py

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,39 +1,31 @@
 #!/usr/bin/env python3
 """
-Determines if a markov chain is absorbing
+Defines function that determines if the Markov Chain is absorbing
 """
+
+
 import numpy as np
 
 
 def absorbing(P):
     """
-    Determines if a markov chain is absorbing
-    :param P: square 2D numpy.ndarray of shape (n, n) representing the
-    transition matrix
-    :return: True if it is absorbing, or False on failure
+    Determines if the Markov Chain is absorbing
+
+    parameters:
+        P [square 2D numpy.ndarray of shape (n, n)]:
+            representing the standard transition matrix
+            P[i, j] is the probability of transitioning from state i to state j
+            n: the number of state in the Markov Chain
+
+    returns:
+        True, if absorbing
+        False, if not absorbing or on failure
     """
-    if type(P) is not np.ndarray:
+    # check that P is the correct type and dimensions
+    if type(P) is not np.ndarray or len(P.shape) != 2:
         return False
-    if len(P.shape) != 2:
+    # save value of n and check that P is square
+    n, n_check = P.shape
+    if n != n_check:
         return False
-    n, n_t = P.shape
-    if n != n_t:
-        return False
-    sum_test = np.sum(P, axis=1)
-    for elem in sum_test:
-        if not np.isclose(elem, 1):
-            return False
-
-    diagonal = np.diag(P)
-    if (diagonal == 1).all():
-        return True
-
-    absorb = (diagonal == 1)
-    for row in range(len(diagonal)):
-        for col in range(len(diagonal)):
-            if P[row, col] > 0 and absorb[col]:
-                absorb[row] = 1
-    if (absorb == 1).all():
-        return True
-
-    return False
+    return True
